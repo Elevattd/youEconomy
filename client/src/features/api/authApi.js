a6ed6d8e9150ddab4122447effbd0bd8bc9e592c
Elevattd <marianoo.14.md@gmail.com>
2022-07-18 00:55:35 -0300

    refactor(client/api RTK): delete console.log()

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -17,7 +17,6 @@ export const authApiSlice = apiSlice.injectEndpoints({
         dispatch(setIsLoading(true));
         try {
           const { data } = await queryFulfilled;
-          console.log("adaasdadasdasdasdasdasd DATA", data);
           dispatch(
             setCredentials({
               user: data.user,
