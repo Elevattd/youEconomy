b7a624be1b09b4f96a17aa1685dbcc9f65271d07
Elevattd <marianoo.14.md@gmail.com>
2022-07-17 22:28:21 -0300

    feat(api): add new controller/route = auth

--- api/src/controllers/auth.js
+++ api/src/controllers/auth.js
@@ -12,28 +12,29 @@ const config = require("../../config");
 const singUp = async (req, res, next) => {
   let user;
   try {
-    const password = await bcrypt.hash(req.body.password, config.rounds);
+    const password = await bcrypt.hash(req.body.password, 10);
     user = { name: req.body.name, email: req.body.email, password };
     req.body = {
       email: user.email,
       password: req.body.password,
     };
-    const registredUser = await getUser("email", user.email);
-    if (registredUser) res.status(400).send("User already exists");
+    const userExists = await getUser("email", user.email);
+    if (userExists) return res.status(400).send(`User already exists`);
     await User.create(user);
-    res.status(201).send({ msg: "Create successfull" });
+    res.sendStatus(201);
   } catch (error) {
     next(error);
   }
 };
+
 const singIn = async (req, res, next) => {
   let user;
   try {
-    user = await await getUser("email", req.body.email);
-    if (!user) res.status(404).send("User not exist");
+    user = await getUser("email", req.body.email);
+    if (!user) return res.status(404).send(`User not found`);
     if (!(await bcrypt.compare(req.body.password, user.password)))
-      res.status(400).send("Incorrent Password");
-    const accessToken = generateAcessToken(user);
+      return res.status(400).send(`Password incorrect`);
+    const accessToken = generateAccessToken(user);
     const refreshToken = await updateRefreshToken(user);
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
@@ -54,11 +55,11 @@ const singIn = async (req, res, next) => {
 
 const handleRefreshToken = async (req, res, next) => {
   const cookies = req.cookies;
-  if (!cookies?.jwt) res.status(401).send("Unauthorized");
+  if (!cookies.jwt) return res.status(401).send(`Unauthorized`);
   const refreshToken = cookies.jwt;
   try {
     const user = await getUser("refreshToken", refreshToken);
-    if (!user) res.status(403).send("Forbiden");
+    if (!user) return res.status(401).send(`Forbiden`);
     let newToken = verifyRefreshToken(user);
     if (typeof newToken === "string") res.send({ accesToken: newToken });
     else res.status(newToken.status).send(newToken.message);
@@ -67,8 +68,51 @@ const handleRefreshToken = async (req, res, next) => {
   }
 };
 
+const handleUserSession = async (req, res, next) => {
+  const cookies = req.cookies;
+  if (!cookies.jwt) return res.status(401).send(`Unauthorized`);
+  const refreshToken = cookies.jwt;
+  try {
+    const user = await getUserFromDbByField("refreshToken", refreshToken);
+    if (!user) return next({ status: 404, message: "Session Inactive" });
+    let newToken = verifyRefreshToken(user);
+    if (typeof newToken === "string") {
+      return res.send({
+        user: {
+          name: user.name,
+          email: user.email,
+          id: user.id,
+        },
+        accessToken: newToken,
+      });
+    } else res.status(newToken.status).send(newToken.message);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const logOut = async (req, res, next) => {
+  const cookies = req.cookies;
+  if (!cookies.jwt) return res.status(401).send(`No token found, unauthorized`);
+  const refreshToken = cookies.jwt;
+  try {
+    const user = await getUserFromDbByField("refreshToken", refreshToken);
+    if (!user) {
+      res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+      return res.sendStatus(204);
+    }
+    await updateRefreshToken(user, true);
+    res.clearCookie("jwt", { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.sendStatus(204);
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   singUp,
   singIn,
   handleRefreshToken,
+  handleUserSession,
+  logOut,
 };
--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,10 +1,18 @@
 const { Router } = require("express");
-const { singUp, singIn, handleRefreshToken } = require("../controllers/auth");
+const {
+  singUp,
+  singIn,
+  handleRefreshToken,
+  handleUserSession,
+  logOut,
+} = require("../controllers/auth");
 const router = Router();
 
 router.post("/singup", singUp);
 router.post("/singin", singIn);
-router.get("/refresh-token", handleRefreshToken);
+router.get("/refresh", handleRefreshToken);
+router.get("/", handleUserSession);
+router.get("/logout", logOut);
 
 module.exports = {
   router,
