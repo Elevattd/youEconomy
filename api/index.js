bc328d9089bc40fb4aac0d7438f4f5e3cf5ee0d2
Elevattd <marianoo.14.md@gmail.com>
2022-08-06 12:16:10 -0300

    fix(api): remove force: true from sequelize

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require("./src/app.js");
 const { sequelize } = require("./src/db");
 const { PORT } = process.env;
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(`LOAD SERVER --> ON: ${PORT}`);
   });
