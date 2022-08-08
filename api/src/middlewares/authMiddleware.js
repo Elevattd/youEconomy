57dfc39512a59429146cb270a3eeded43c51c5cc
Elevattd <marianoo.14.md@gmail.com>
2022-08-08 10:47:48 -0300

    fix(api): authmiddleware

--- api/src/middlewares/authMiddleware.js
+++ api/src/middlewares/authMiddleware.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-// authenticate the token send by header on the request
-
-const authenticateToken = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  const bearer = req.headers.authorization?.split(" ")[0];
-  if (!token | (bearer !== "Bearer"))
-    return res.status(401).send(`No token found, unauthorized`);
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.status(401).send(`No token found, unauthorized`);
+  const baerer = req.headers.authorization?.split(" ")[0];
+  if (!token || baerer !== "Bearer")
+    return res.status(401).send("You need a valid token to access this route");
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error)
+      return res
+        .status(403)
+        .send("You don`t have access. Token no longer valid");
     req.body.user = decoded.UserInfo;
     next();
   });
 };
 
 module.exports = {
-  authenticateToken,
+  authMiddleware,
 };
