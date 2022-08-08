1b70a82bbfe53183d9fca2c60940e966c10ee6e1
Elevattd <marianoo.14.md@gmail.com>
2022-08-08 10:36:54 -0300

    feat(api): add localhost for cors

--- api/src/middlewares/authMiddleware.js
+++ api/src/middlewares/authMiddleware.js
@@ -1,21 +1,20 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
+// authenticate the token send by header on the request
+
+const authenticateToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
-  const baerer = req.headers.authorization?.split(" ")[0];
-  if (!token || baerer !== "Bearer")
-    return res.status(401).send("You need a valid token to access this route");
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-    if (error)
-      return res
-        .status(403)
-        .send("You don`t have access. Token no longer valid");
+  const bearer = req.headers.authorization?.split(" ")[0];
+  if (!token | (bearer !== "Bearer"))
+    return res.status(401).send(`No token found, unauthorized`);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.status(401).send(`No token found, unauthorized`);
     req.body.user = decoded.UserInfo;
     next();
   });
 };
 
 module.exports = {
-  authMiddleware,
+  authenticateToken,
 };
